36af35938769fe12e407830ec65c15b7455a3166
Guilherme Godoi <guiggff@gmail.com>
2021-01-08 15:54:10 -0300

    wiseGoat

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+function init() {
+  const botaoConselho = document.getElementById("proximo");
+  botaoConselho && botaoConselho.addEventListener("click", clickBtn);
+
+  var frase = document.getElementById("paragrafo");
+  frase && pegarFrase(frase);
+
+  var restart = document.getElementById("restart");
+  restart && restart.addEventListener("click", re);
+}
+
+function clickBtn(e) {
+  e.preventDefault();
+  window.location = "/conselho.html";
+}
+
+function pegarFrase(frase) {
+  fetch("https://api.adviceslip.com/advice")
+    .then((response) => response.json())
+    .then((conselhoBode) => {
+      frase.innerHTML = conselhoBode.slip.advice;
+    });
+}
+
+function re() {
+  location.reload();
+}
+
+init();
